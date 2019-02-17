4c156ab4b88b864e4823aa313a4e093ddf867399
Chris Evans <Cevans@peerlessproducts.com>
2019-02-17 00:04:29 -0600

    added tests

--- Chess/Library.fs
+++ Chess/Library.fs
@@ -1,5 +1,18 @@
 namespace Chess
-
+open FSharp.Core
 module Say =
     let hello name =
         printfn "Hello %s" name
+
+module Pieces =
+    type Range = int * int
+    type Movement = L | Foward of Option<Range> | Back of Option<Range> | Right of Option<Range> | Left of Option<Range> 
+                    | ForwardRightDiag of Option<Range>
+                    | ForwardLeftDiag of Option<Range> | BackRightDiag of Option<Range> | BackLeftDiag of Option<Range>
+    type Color = Black | White
+    type PieceLocation = {X:int;Y:int;Color:Color;Moves:Movement list;Attacks:Movement list}
+    let range from tto = Some (Range(from,tto))
+    let pawnStart color x y  = {X=x;Y=y;Color=color;Moves=[Foward (range 1 2)];Attacks=[ForwardLeftDiag (range 1 1);ForwardRightDiag (range 1 1)]}
+    
+module Board =
+    0
--- Tests/Program.fs
+++ Tests/Program.fs
@@ -1,8 +1,24 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Fuchu
+open NUnit
+open NUnit.Framework
+open Chess
+open Pieces
+
+let ChessTests =
+    testList "this is a test" 
+            [
+                testCase "this is true" <| fun _ -> Assert.AreEqual(0,0)
+                testCase "pawn created" <| fun _ -> 
+                                               let pawn = pawnStart White 0 0
+                                               Assert.AreEqual(White,pawn.Color)
+                                               Assert.AreEqual(2, pawn.Attacks.Length)
+            ]
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let result = run ChessTests
+    Console.ReadLine() |> ignore
+    result
